e0d4b5d025e6187b52e358b871c300ab641f8b8b
wmyq <644452492@qq.com>
2021-12-12 15:35:13 +0800

    start

--- src/test/java/com/cy/store/StoreApplicationTests.java
+++ src/test/java/com/cy/store/StoreApplicationTests.java
@@ -1,13 +1,23 @@
 package com.cy.store;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class StoreApplicationTests {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void getConnection(){
+        System.out.println(dataSource.getClass());
+    }
 }
